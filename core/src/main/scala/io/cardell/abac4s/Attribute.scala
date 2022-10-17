19ee54063dd718ac4b6cfd7922c29ec6596408a4
Alex Cardell <29524087+alexcardell@users.noreply.github.com>
2022-10-17 19:24:23 +0100

    Add CI Release config (#2)

--- core/src/main/scala/io/cardell/abac4s/Attribute.scala
+++ core/src/main/scala/io/cardell/abac4s/Attribute.scala
@@ -9,8 +9,21 @@ object Attribute {
   type Key = String
   type Value = String
 
+  /** Attributes about the user/service authorizing
+    */
   case class Subject(key: K, value: V) extends Attribute
+
+  /** Attributes about the resource the subject is trying to access. Likely to
+    * be derived after pre-fetching the resource without exposing to the
+    * subject.
+    */
   case class Resource(key: K, value: V) extends Attribute
+
+  /** Attributes about the action the subject is trying to take on the resource.
+    */
   case class Action(key: K, value: V) extends Attribute
+
+  /** Attributes covering any environment or external state of the system.
+    */
   case class Context(key: K, value: V) extends Attribute
 }
