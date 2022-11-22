44737847382d16663dbbb8a47939185128f9b77e
alexcardell <29524087+alexcardell@users.noreply.github.com>
2022-11-22 17:23:38 +0000

    Fix syntax imports

--- core/src/main/scala/io/cardell/abac4s/syntax/package.scala
+++ core/src/main/scala/io/cardell/abac4s/syntax/package.scala
@@ -2,4 +2,6 @@ package io.cardell.abac4s
 
 package object syntax
     extends syntax.AttributeSourceSyntax
+    with syntax.PolicySyntax
     with syntax.PolicyComposeSyntax
+    with syntax.PolicyResultFSyntax
