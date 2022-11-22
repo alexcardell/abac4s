c88c2bbe7feff9e4d4dacd8137fab1a474622da7
Alex Cardell <29524087+alexcardell@users.noreply.github.com>
2022-11-22 12:22:17 +0000

    Add Policy.all_ combinator (#3)

--- core/src/main/scala/io/cardell/abac4s/syntax/PolicyComposeSyntax.scala
+++ core/src/main/scala/io/cardell/abac4s/syntax/PolicyComposeSyntax.scala
@@ -1,13 +1,8 @@
 package io.cardell.abac4s.syntax
 
 import cats.Monad
-import cats.data.Validated.Invalid
-import cats.data.Validated.Valid
-import cats.implicits._
 
 import io.cardell.abac4s.Policy
-import io.cardell.abac4s.PolicyResult
-import io.cardell.abac4s.PolicyResult.Granted
 
 trait PolicyComposeSyntax {
   implicit def policyAndSyntax[F[_]: Monad, A](
@@ -17,16 +12,9 @@ trait PolicyComposeSyntax {
 
 final class PolicyAndOps[F[_]: Monad, A](policy: Policy[F, A]) {
 
-  def and[B](other: Policy[F, B]): Policy[F, (A, B)] = new Policy[F, (A, B)] {
+  def and[B](other: Policy[F, B]): Policy[F, (A, B)] =
+    Policy.and(policy, other)
 
-    def run(): F[PolicyResult[(A, B)]] = {
-      (policy.run(), other.run()).tupled.map {
-        case (Valid(a), Valid(b))            => Granted((a, b))
-        case (left @ Invalid(_), Valid(_))   => left
-        case (Valid(_), right @ Invalid(_))  => right
-        case (Invalid(left), Invalid(right)) => Invalid(left ++ right)
-      }
-    }
-
-  }
+  def and_(other: Policy[F, A]): Policy[F, A] =
+    Policy.and_(policy, other)
 }
